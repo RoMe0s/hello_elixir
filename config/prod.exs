d518554774b6e1a839ff2ebd69927ff7bcfd211d
RoMe0s <RoMe0s@yandex.ru>
2017-09-13 19:48:18 +0300

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -63,6 +63,6 @@ config :logger, level: :info
 # Configure your database
 config :hello_elixir, HelloElixir.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env('DATABASE_URL')
+  url: System.get_env('DATABASE_URL'),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
