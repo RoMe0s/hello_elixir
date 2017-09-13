9d23b0066ce9e1fe2bc2ba3219e8894f737f368e
RoMe0s <RoMe0s@yandex.ru>
2017-09-13 19:43:58 +0300

    init

---
+++ test/hello_elixir_web/views/error_view_test.exs
@@ -0,0 +1,21 @@
+defmodule HelloElixirWeb.ErrorViewTest do
+  use HelloElixirWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(HelloElixirWeb.ErrorView, "404.html", []) ==
+           "Page not found"
+  end
+
+  test "render 500.html" do
+    assert render_to_string(HelloElixirWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(HelloElixirWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
